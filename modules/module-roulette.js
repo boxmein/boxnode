ad2cd99d3265f88fa54a63cf48b5cdda0c3e9fe7
boxmein <wsdf294@gmail.com>
2015-02-16 16:04:57 +0200

    russian roulette gracefully defaults, also now hopefully works :D

--- modules/module-roulette.js
+++ modules/module-roulette.js
@@ -1,6 +1,22 @@
+/*
+  Russian Roulette :D
+*/
+
+var cfg, underscore = require('underscore');
+
 exports.type = 'command';
 
-var cfg;
+var DEFAULT_CHANCES = {
+  'russian': 1/6.0,
+  'chancekick': 0.05,
+  'chancekick_backfire': 0.25
+};
+
+var DEFAULT_PUNISHMENT = {
+  'type': 'kick',
+  'ban_duration': 0
+};
+
 
 exports.listAll = function() {
   return ['*', 'russian', 'chancekick'];
@@ -14,6 +30,22 @@ exports.getHelp = function() {
   };
 };
 
+function punish(line, target, respond) {
+  target = target || '*!*@' + line.hostname;
+  if (cfg.punishment.type.indexOf('ban') !== -1) {
+    respond.MODE(line.params[0], '+b', target);
+
+    setTimeout(function() {
+      respond.MODE(line.params[0], '-b', target);
+    }, cfg.punishment.ban_duration);
+
+  }
+
+  if (cfg.punishment.type.indexOf('kick') !== -1) {
+    respond.RAW('KICK ' + line.params[0] + ' ' + target);
+  }
+}
+
 exports.listener = function(line, words, respond) {
   var split = this.event.split('.');
 
@@ -25,7 +57,8 @@ exports.listener = function(line, words, respond) {
   switch (split[1]) {
     case 'russian':
       if (Math.random() < cfg.chances.russian) {
-        respond('Bang! TODO: add consequences.');
+        respond('Bang!');
+        punish(line, undefined, respond);
       } else {
         respond('The gun clicks.');
       }
@@ -37,9 +70,11 @@ exports.listener = function(line, words, respond) {
         return;
       }
       if (Math.random() < cfg.chances.chancekick) {
-        respond('Booted the target (TODO: add consequence): ' + words[1]);
-      } else if (Math.random() < cfg.chances.chancekick_recoil) {
-        respond('Backfired! (TODO: kick you)');
+        respond('Booted the target: ' + words[1]);
+        respond.RAW('KICK ' + line.params[0] + ' ' + words[1]);
+      } else if (Math.random() < cfg.chances.chancekick_backfire) {
+        respond('Backfired!');
+        respond.RAW('KICK ' + line.params[0] + ' ' + line.nick);
       }
       break;
     default: break;
@@ -48,4 +83,10 @@ exports.listener = function(line, words, respond) {
 
 exports.init = function(config, myconfig, alias) {
   cfg = myconfig;
+
+  cfg.chances = _.defaults(cfg.chances, DEFAULT_CHANCES);
+  cfg.punishment = _.defaults(cfg.punishment, DEFAULT_PUNISHMENT);
+
+  alias('rr', 'roulette.russian');
+  alias('k', 'roulette.chancekick');
 };
