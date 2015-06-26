e649fa0dac8c8a843450f71b105aeee932a409a3
boxmein <wsdf294@gmail.com>
2015-06-26 17:00:42 +0300

    VHostAssigned is also now a neat message

--- modules/module-shinylogging.js
+++ modules/module-shinylogging.js
@@ -12,7 +12,7 @@ logger.properties.colors.INFO = '37;1';
 // events for which we'll just log all parameters
 var ircEventParamLog = ['ISupport', 'MOTD', 'EndOfMOTD', 'LUserChannels',
     'LUserClient', 'LUserMe', 'MyInfo', 'Welcome', 'YourHost', 'NamReply',
-    'EndOfNames', 'Created'];
+    'EndOfNames', 'Created', 'VHostAssigned'];
 
 exports.init = function(app, irc, command, u) {
 
