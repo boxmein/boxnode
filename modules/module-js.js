a6034fb343233f124fc7d158fda85485e1e41eb1
boxmein <wsdf294@gmail.com>
2015-06-26 18:04:01 +0300

    new module: sandboxed javascript

---
+++ modules/module-js.js
@@ -0,0 +1,31 @@
+var Sandbox = require('sandbox');
+var logger = new require('toplog')({concern: 'js', loglevel: 'VERBOSE'});
+var box = new Sandbox();
+
+exports.type = 'command';
+exports.listAll = function() {
+  return ['*'];
+};
+
+exports.getHelp = function() {
+  return {
+    '*': '`js <code...>` - runs Javascript in a closure. (function() { return <code>; })()'
+  };
+};
+
+exports.listener = function(line, words, respond, util) {
+  box.run(words.slice(1).join(' '), function(out) {
+    var endstr = '';
+
+    logger.verbose('result:',out);
+
+    if (out.console && out.console.length > 0)
+      endstr += ' Console: ' + out.console.join('\\n');
+
+    if (out.result)
+      endstr += ' Return: ' + out.result;
+
+    respond(endstr);
+  });
+};
+
