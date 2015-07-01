8cd70bfa726a17063bc50a4a629aea4a604d8965
boxmein <wsdf294@gmail.com>
2015-07-01 22:46:16 +0300

    new module: is a website up?

---
+++ modules/module-isup.js
@@ -0,0 +1,48 @@
+var net = require('net');
+var logger = new require('toplog')({concern: 'isup', loglevel: 'INFO'});
+
+exports.type = 'command';
+
+exports.listAll = function() {
+  return ['*'];
+};
+
+exports.getHelp = function() {
+  return {
+    '*': '`isup <domain>` - Check if a website is up. (up = accepts connections on port 80)'
+  };
+};
+
+exports.listener = function(line, words, respond, util) {
+  var domain = words[1];
+  if (!domain) {
+    return respond('you didn\'t tell me a domain! try `help isup`!');
+  }
+
+  logger.verbose('connecting via unref-d socket to port 80 of', domain);
+  var sock = net.connect(80, domain, function() {
+    logger.debug(domain+':80 up');
+    respond('Seems up to me!');
+    sock.end();
+  });
+
+  sock.unref();
+  sock.setTimeout(5000);
+
+  sock.on('timeout', function() {
+    logger.debug(domain+':80 did not respond in 5000ms');
+    respond('It\'s down (timeout)!');
+    sock.end();
+  });
+
+  sock.on('error', function(err) {
+    logger.error(err);
+    if (err.errno == 'ENOTFOUND') {
+      respond('invalid address!');
+    } else if (err.errno == 'ECONNREFUSED') {
+      respond('It\'s down (connection refused)!');
+    } else {
+      respond('error handling request! :(((');
+    }
+  });
+};
